db479117892d888dfc79ace1dc30091e64b7586c
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-20 13:59:20 -0400

    Updated from Brython Server: 9/20/2017 1:59:20 PM

--- house.py
+++ house.py
@@ -1,1 +1,11 @@
+#Charlie Goodrich
+#09/20/17
+#house.py - makes a house
 
+from ggame import *
+
+white = Color(0xFFFFFF, 1)
+brown = Color(0xD2691E, 1)
+black = Color(0x000000, 1)
+
+whiteHouse = RectangleAsset(100, 200,
