88236e68935eb09de650e46e2229cbeff760ee84
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-22 14:54:12 -0400

    Updated from Brython Server: 9/22/2017 2:54:12 PM

--- israel.py
+++ israel.py
@@ -5,15 +5,15 @@
 from ggame import *
 
 blue = Color(0x0000FF, 1)
-white = Color(0xFFFFFF, 1)
+white = Color(0xFFFFFF, 0)
 black = Color(0x000000, 1)
 
 blueOutline = LineStyle(1, blue)
 
 whiteBackground = RectangleAsset(300, 200, blueOutline, white)
 blueRect = RectangleAsset(300, 45, blueOutline, blue)
-star1 = PolygonAsset([(150, 56.6987),  (100, 143.3013),  (200, 143.3013)], blueOutline, blue)
-star2 = PolygonAsset([(100, 66.6987), (150, 153.3013), (200, 66.6987)], blueOutline, blue)
+star1 = PolygonAsset([(150, 58.0385),  (120, 131.9615),  (180, 131.9615)], blueOutline, white)
+star2 = PolygonAsset([(120, 78.0385), (150, 131.9615), (180, 78.0385)], blueOutline, white)
 Sprite(whiteBackground)
 Sprite(blueRect)
 Sprite(blueRect, (0, 155))
