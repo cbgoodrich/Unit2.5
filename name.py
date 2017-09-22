06272c44273afd27c1ac221ba8e983d5b8635008
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-21 20:48:31 -0400

    Updated from Brython Server: 9/21/2017 8:48:30 PM

--- name.py
+++ name.py
@@ -2,5 +2,14 @@
 #09/20/17
 #name.py - prints out user's name
 
+from ggame import *
+
 name = input("Enter your name: ")
-color_input = input("Enter a hex code for a color: ")
+color = input("Enter a hex code for a color (Enter '0x' then the color code): ")
+
+color = Color(color, 1)
+
+text = TextAsset(name, fill = color, style = "bold 48pt Times")
+
+Sprite(text, (100, 100))
+App().run()
