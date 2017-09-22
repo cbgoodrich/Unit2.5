70c41ee8a413f605c686c74a349c3fd30a6fcd32
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-21 21:00:02 -0400

    Updated from Brython Server: 9/21/2017 9:00:01 PM

--- yield.py
+++ yield.py
@@ -1,1 +1,18 @@
+#Charlie Goodrich
+#09/22/17
+#yield.py - displays a yield sign
 
+from ggame import *
+
+red = Color(0xFF0000, 1)
+white = Color(0xFFFFFF, 1)
+black = Color(0x000000, 1)
+outline = LineStyle(1, black)
+
+redTriangle = PolygonAsset([(50, 50), (150, 50), (100, 136.6025)], outline, red)
+whiteTriangle = PolygonAsset([(70, 65), (130, 65), (100, 116.9615)], outline, white)
+
+Sprite(redTriangle)
+Sprite(whiteTriangle)
+
+App().run()
