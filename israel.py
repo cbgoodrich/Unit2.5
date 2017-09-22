0cfc0abe2a651d2a0754ed7182b94c91400d2f85
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-22 14:48:54 -0400

    Updated from Brython Server: 9/22/2017 2:48:53 PM

--- israel.py
+++ israel.py
@@ -8,16 +8,17 @@ blue = Color(0x0000FF, 1)
 white = Color(0xFFFFFF, 1)
 black = Color(0x000000, 1)
 
-blackOutline = LineStyle(1, black)
+blueOutline = LineStyle(1, blue)
 
-whiteBackground = RectangleAsset(300, 200, blackOutline, white)
-blueRect = RectangleAsset(300, 45, blackOutline, blue)
-star = PolygonAsset([(150, 56.6987), (100, 143.3013), (200, 143.3013), (100, 56.6987), (200, 56.6987), (150, 143.3013)], blackOutline, blue)
-
+whiteBackground = RectangleAsset(300, 200, blueOutline, white)
+blueRect = RectangleAsset(300, 45, blueOutline, blue)
+star1 = PolygonAsset([(150, 56.6987),  (100, 143.3013),  (200, 143.3013)], blueOutline, blue)
+star2 = PolygonAsset([(100, 66.6987), (150, 153.3013), (200, 66.6987)], blueOutline, blue)
 Sprite(whiteBackground)
 Sprite(blueRect)
 Sprite(blueRect, (0, 155))
-Sprite(star)
+Sprite(star1)
+Sprite(star2)
 
 App().run()
 
