2113facaadb5c9a5396ebfa1dd4f2f0283220e04
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-22 14:25:22 -0400

    Updated from Brython Server: 9/22/2017 2:25:22 PM

--- name.py
+++ name.py
@@ -5,11 +5,16 @@
 from ggame import *
 
 name = input("Enter your name: ")
-color = input("Enter a hex code for a color (Enter '0x' then the color code): ")
+color_code = input("Enter a hex code for a color (Enter '0x' then the color code): ")
+black = Color(0x000000, 1)
+color = Color(color_code, 1)
 
-color = Color(color, 1)
+blackOutline = LineStyle(1, black)
 
+background = RectangleAsset(1000, 1000, blackOutline, color)
 text = TextAsset(name, fill = color, style = "bold 48pt Times")
 
+
+Sprite(background)
 Sprite(text, (100, 100))
 App().run()
