a1f7ce4436bd8f93bd6517c4ea2a9a3681204c46
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-26 14:30:37 -0400

    Updated from Brython Server: 9/26/2017 2:30:36 PM

--- stop.py
+++ stop.py
@@ -1,1 +1,19 @@
+#Charlie Goodrich
+#09/27/17
+#stop.py - makes a stop sign
 
+from ggame import *
+from math import sqrt
+
+red = Color(0xFF0000, 1)
+white = Color(0xFFFFFF, 1)
+
+redOutline = LineStyle(1, red)
+
+stopSign = PolygonAsset([(100, 0), (200, 0), (300, 100), (300, 200), (200, 300), (100, 300), (0, 200), (0, 100)], redOutline, red)
+text = TextAsset("STOP", fill = white, style = "50pt Times")
+
+
+Sprite(stopSign)
+Sprite(text, (72.5, 122.5))
+App().run()
