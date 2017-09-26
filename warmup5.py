dc368775066d1b1e9ae34cf9a739bb1dc9b94b78
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-26 14:16:42 -0400

    Updated from Brython Server: 9/26/2017 2:16:41 PM

--- warmup5.py
+++ warmup5.py
@@ -1,1 +1,19 @@
+#Charlie Goodrich
+#09/27/17
+#warmup5.py - getting warm
 
+from ggame import *
+
+blue = Color(0x0000FF, 1)
+yellow = Color(0xFFFF00, 1)
+black = Color(0x000000, 1)
+
+blackOutline = LineStyle(1, black)
+
+yellowDiamond = PolygonAsset([(50, 25), (100, 75), (50, 125), (0, 75)], blackOutline, yellow)
+blueName = TextAsset("Charlie", fill = blue, style = "12pt Times")
+
+Sprite(yellowDiamond)
+Sprite(blueName, (15, 70))
+
+App().run()
